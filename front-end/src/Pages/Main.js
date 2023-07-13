19ab420850f90dae9045c4366ada588e5ae4c4fe
isabellaMas <icm268@nyu.edu>
2023-07-13 15:45:56 -0300

    Image with text overlay

--- front-end/src/Pages/Main.js
+++ front-end/src/Pages/Main.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Header from "../Components/Header";
 import "./Main.css";
-
+import mainImage from "../Images/mainImage.jpg";
 const MainPage = () => {
   return (
     <div>
       <div className="menu">
-        <Header style={{ color: "D9D2B6" }}/>
+        <Header style={{ color: "D9D2B6" }} />
       </div>
-      <div className="intro">
-        <div id="hello">HELLO, I'M</div>
-        <div id="first">ISABELLA</div>
-        <div id="last">MASIERO</div>
+      <div className="imgAndName">
+        <div className="intro">
+          <div id="hello">HELLO, I'M</div>
+          <div id="first">ISABELLA</div>
+          <div id="last">MASIERO</div>
+        </div>
+        <img id="gradPic" src={mainImage} alt="graduation"></img>
       </div>
     </div>
   );
